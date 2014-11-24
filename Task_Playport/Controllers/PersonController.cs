f36dd640de98fefc12b85a5db3c947d861929064
goldsmit409 <goldsmit409@outlook.com>
2014-11-24 11:35:06 +0530

    Code

---
+++ Task_Playport/Controllers/PersonController.cs
@@ -0,0 +1,28 @@
+﻿using Task_Playport.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Net.Http;
+using System.Text;
+
+namespace Task_Playport.Controllers
+{
+    [RoutePrefix("api")]
+    public class PersonController : ApiController
+    {
+        [Route("person/{Name}/hello/{Location}")]
+        public HttpResponseMessage GetResponse(string Name, string Location)
+        {
+            string result = "";
+            if (Name == null || Location == null)
+                result = "Error, Try again";
+            else
+                result = "Hello " + Name + " from " + Location;
+            var resp = new HttpResponseMessage(HttpStatusCode.OK);
+            resp.Content = new StringContent(result, Encoding.UTF8, "text/plain");
+            return resp;
+        }
+    }
+}
---
+++ Task_Playport/Models/DataFetch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Task_Playport.Models
+{
+    public class DataFetch
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+    }
+}
