cbef629b964b670c2fef9a43dbcf1dfc69b5fe95
goldsmit409 <goldsmit409@outlook.com>
2014-11-25 14:18:06 +0530

    Update 1
    added movie method

---
+++ Task_Playport/Controllers/MovieController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Task_Playport.Models;
+
+namespace Task_Playport.Controllers
+{
+    [RoutePrefix("api")]
+    public class MovieController : ApiController
+    {
+        Movie[] movies = new Movie[] 
+        { 
+            new Movie { title = "Interstellar", language = "English", durationIsSecs = 7245}, 
+            new Movie { title = "Annabelle", language = "English", durationIsSecs = 6482 }, 
+            new Movie { title = "Haider", language = "Hindi", durationIsSecs = 8767 }, 
+        };
+        
+        
+        [Route("movie/list")]
+        public IEnumerable<Movie> GetMovieList()
+        {
+            return movies;
+        }
+        
+        [Route("movie/List/{Name}")]
+        public IHttpActionResult GetMovie(string Name)
+        {
+            var movie = movies.FirstOrDefault((m) => m.title == Name);
+            if (movie == null)
+                return NotFound();
+            return Ok(movie);
+
+        }
+    }
+}
---
+++ Task_Playport/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Playport.Models
+{
+    public class Movie
+    {
+        public string title { get; set; }
+        public string language { get; set; }
+        public int durationIsSecs { get; set; }
+    }
+}
